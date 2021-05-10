a9231ecba4d0241bdc3d4f948a50fdef4cd61188
Leon <leon.chow1@ontariotechu.net>
2021-05-10 11:17:02 -0400

    Fix functionality, works in one file right now, but will later refactor to work with child components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
-import Header from './components/header/Header';
-import Body from './components/body/Body';
-import Footer from './components/footer/Footer';
 import React from 'react';
+
+/*import Header from './components/header/Header';
+import Body from './components/body/Body';
+import Footer from './components/footer/Footer';*/
 
 class App extends React.Component {
   constructor() {
@@ -13,14 +13,53 @@ class App extends React.Component {
       cats: 0,
       dogs: 0,
     }
+
+    this.reset = this.reset.bind(this);
+    this.onIncrement = this.onIncrement.bind(this);
   }
+
+  onIncrement(pet) {
+    if (pet === 'dogs') {
+      this.setState(prevState => {
+        return {
+          dogs: prevState.dogs + 1,
+        }
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          cats: prevState.cats + 1,
+        }
+      });
+    }
+  }
+
+  reset() {
+    console.log("reset");
+    this.setState({cats: 0, dogs: 0}); 
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
-        <Footer votes={this.state}/>
+        <div className="header">
+            <h1> Cats and Dogs Voting App </h1>
+        </div>
+        <div className="body">
+          <button onClick={() => this.onIncrement('cats')}> Cats </button>
+          <button onClick={() => this.onIncrement('dogs')}> Dogs </button>
+          <button onClick={() => this.reset()}> Reset </button>
+        </div>
+        <div className="footer">
+          <h1> Cats - {this.state.cats} | Dogs - {this.state.dogs} </h1>
+        </div> 
       </div>
+      /*<div className="App">
+        <Header />
+        <Body votes={this.state}/>
+        <Footer votes={this.state}/>
+      </div>*/
+
     );
   }
 }
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
 export default class Body extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dogs: 0,
+            cats: 0,
+        }
+
+        this.reset = this.reset.bind(this);
+        this.onIncrement = this.onIncrement.bind(this);
+    }
+
     render() {
         return (
             <div className="body">
-                <button> Cats </button>
-                <button> Dogs </button>
-                <button> Reset </button>
+                <button onClick={() => this.onIncrement('cats')}> Cats </button>
+                <button onClick={() => this.onIncrement('dogs')}> Dogs </button>
+                <button onClick={() => this.reset}> Reset </button>
             </div>
         );
+    }
+
+    onIncrement(pet) {
+        if (pet === 'dogs') {
+            this.setState(prevState => {
+                return {
+                    dogs: prevState.dogs + 1,
+                }
+            });
+        } else {
+            this.setState(prevState => {
+                return {
+                    cats: prevState.cats + 1,
+                }
+            });
+        }
+    }
+
+    reset() {
+        console.log("reset");
+        this.setState({cats: 0, dogs: 0}); 
     }
 }
